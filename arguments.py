cf54d3164041add1f3ef3f685f3bc4beb7cfb5b8
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-03-28 21:54:32 -0600

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -1,39 +1,38 @@
 #!/bin/env python3
 
-from arguments import args
+import argparse
 
-'''
+help_menu = '''
 
-Classes for Traffic Monitor
-
-'''
+	PLACEHOLDER FOR ASCII ART
 
 
-class Snoopie():
-# Monitor network traffic and copy IPs, Frequency, and DataSize into a hidden file @ PWD
+	-h, --help :: Show this menu
 	
-	def __init__(self):
-		# Create dict to hold data, this dict will be output into other classes
-		self.doghouse = {}
-		
-		# Label the location we will be monitoring
-		# Using a .txt file with a few lines of foo for now to test the logic
-		self.location = 'testfile.txt'
-		
-		# Temporary storehouse for incoming data
-		self.harLis = []
-		
-	# Create method to bring data in from location
-	def harvest(self):
-		with open(self.location, 'r+') as f:
-		
-			# Read each line into the temporary working file
-			# Strip the newline char
-			self.harLis = [x.strip() for x in f.readlines()]
-			
-			# Reset pointer
-			f.seek(0)
-			
-		# Debug Test
-		if args.debug:
-			print(f'\n {self.harLis} \n')
+	--debug :: Shows various developing cues used in production
+	
+	
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+### ARGUMENT PARSER ###
+
+# build argument parser
+parser = argparse.ArgumentParser(add_help=False)
+
+parser.add_argument('-h', '--help', action='store_true')
+parser.add_argument('--debug', action='store_true')
+
+# parse passed argumentation
+args = parser.parse_args()
+
