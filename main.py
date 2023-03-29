cded98a2b90b13b4535fa04329f497891b8cc7d3
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-03-28 21:55:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,18 @@
 ### IMPORTS ###
 
 from arguments import args
+import classes as c
 
 ### END IMPORTS ###
 
+
+
+
+### Production Testing ###
+
+# Test Snoopie Class
+snoopie = c.Snoopie()
+snoopie.harvest()
+
+
+### END PRODUCTION TESTING ###
