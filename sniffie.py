fd0aa22b3f7fa9c4450f9dbd382b9702f3e7355f
cj <cj@kali.hitronhub.home>
2023-03-29 22:21:36 -0600

    Added capture and is_private method
    I added is_private method to check weather or not an IP is private, and also added a capture method for capturing live packets

---
+++ sniffie.py
@@ -0,0 +1,54 @@
+from classes import Snoopie
+from netaddr import IPAddress #pip install netaddr
+import pyshark
+
+#sudo apt install tshark
+
+'''
+
+There are two different is_private() method, one is from the IPAddress class
+Note: I couln't come up with a better name, but feel free to change it.
+
+'''
+
+class Sniffie(Snoopie):
+    def __init__(self):
+        super().__init__()
+        ''' using this right now for testing purposes as of right now as a place holder 
+        of the actual IP's comming from the file'''
+        #self.ipadress = ipadress
+        
+        #Applying a time out
+        self.Timout = 5
+
+        #number of packet count
+        self.PacketCount = 15
+
+        #
+        self.ifaceName = 'wlan0'
+        self.filter_traffic = 'port 443'
+    
+
+    def is_private(self):
+        #cheking to see if the ipaddr is private or not
+        if IPAddress(self.IP).is_private():
+            return True
+
+        return False
+
+    #create method to capture network traffic
+    def capture(self):
+        #capturing live packet, then assign to cap(capture) variable
+        cap = pyshark.LiveCapture(interface=self.ifaceName, bpf_filter=self.filter_traffic)
+        cap.sniff(timeout=self.Timout, packet_count=self.PacketCount)
+
+        if len(cap) >=1:
+            for packet in cap:
+                return f'Src: {packet.ip.src} \nDst: {packet.ip.dst}'
+    
+
+x = Sniffie()
+
+res = x.capture()
+
+print(res)
