cd8ea0b59ab289d11da07f1fdf7383c0bb6a2ae3
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-03-31 22:11:47 -0600

    Update classes.py
    Updated the harvest method, now returns a list of dictionaries, each list item is a connection, and the dictionaries describe features about each connection

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 from arguments import args
+import helpers as help
 from netaddr import IPAddress
 import pyshark, psutil
 
@@ -22,17 +23,28 @@ class Snoopie():
 		# Using a .txt file with a few lines of foo for now to test the logic
 		self.location = '/dev/net/' + args.interface
 		
-		# Temporary storehouse for incoming data
+		# Temporary storehouse for initial incoming data
 		self.raw_harvest = []
+		
+		# Refine and take control of the data
+		self.refined_harvest = []
+		
 		
 	# Create method to bring data in from location
 	def harvest(self):
-		# get all active net connections returned in named tuple
+		# get all active net connections returned in a list of named tuples
 		# includes addressFamily, tcp/udp, local_addr, remote_addr, conn_status, process_id
 		self.raw_harvest = psutil.net_connections()
+		
+		# Unpack the tuple, take control of data, and customize output
+		for connection in self.raw_harvest:
+			self.refined_harvest.append(help.transform_tuple(connection))
+			
+		return self.refined_harvest
+	
+		
 			
 		# Debug Test
 		if args.debug:
 			for item in self.raw_harvest:
 				print('\r\n' + str(item))
-			
