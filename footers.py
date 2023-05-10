7b1909a51a95479cc89c2c050ea6f7e33f1a8ebf
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-05-09 20:25:40 -0600

    Update footers.py
    Added authors and story

---
+++ footers.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+
+authors = '''
+
+	cj_
+	
+		Github: cj-509
+		
+	7R!Xx
+	
+		Twitter: @7RiXxSec
+		
+		Github: 7RIXx
+		
+	'''
+	
+story = '''
+
+	One dark and stormy day, 7R!Xx was troubleshooting the 'cyberS' code, perched on a Starbucks barstool.
+	
+	Suddenly an interruption from over the shoulder revealed cj_, asking about the code up on the screen.
+	
+	After a brief coffee and introduction, the two decided to write something together, and a monitoring
+	
+	tool seemed to be the most appropriate suggestion that came forth. Enter Network Monitoring Tool.
+	
+	
+	'''
