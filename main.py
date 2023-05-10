765de016131b8312fade2619e7c8897ce8ad0e67
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-05-09 20:30:23 -0600

    Update main.py
    Sub-final frame fleshed out, just have to plug in Sniffie when complete

--- main.py
+++ main.py
@@ -5,26 +5,63 @@
 
 from arguments import args
 import classes as c
-from sniffie import Sniffie
+import helpers as h
+import headers as hd
+from time import sleep
+#from sniffie import Sniffie
 ### END IMPORTS ###
 
 
 
+### MAIN EXECUTION SEQUENCE ###
 
-### Production Testing ###
-
-# Test Snoopie Class
-snoopie = c.Snoopie()
-for item in snoopie.harvest():
-	print('\n\n')
-	print(item)
+# Make it fancy
+if not args.quick:
+	print(hd.pretty_banner + '\n\n')
+	
 
 
+# Snoopie Class Call
+if args.snoop:
 
+	if not args.quick:
+		print(' Harvesting data ... \n\n')
 
-test = Sniffie()
-print(test.is_dns_resolvable())
-print(test.is_ip_private())
+	snoopie = c.Snoopie()
 
+	snoopie.harvest()
+	
+	if not args.quick:
+		print('\n\n Analyzing data ... \n\n')
 
-### END PRODUCTION TESTING ###
+	snoopie.analyze()
+	
+	if not args.quick:
+		print('\n\n Reporting suspicious listings ... \n\n')
+
+	snoopie.report()
+
+# End Snoopie Class Call
+
+# Sniffie Class Call
+elif args.sniff:
+
+	packets = Sniffie()
+
+	for packet in packets.capture_network_traffic():
+		print(f'\n{packet}')
+		
+	'''
+	
+	NEED SNIFFIE EXECUTION SEQUENCE IN HERE
+	
+	'''
+	
+	
+# End Sniffie Class Call
+
+else:
+
+	print(hd.help_menu)
+
+### END MAIN EXECUTION SEQUENCE ###
