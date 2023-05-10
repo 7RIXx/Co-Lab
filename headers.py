ab36317c4fb84f93c2bf27b6f8f356166a7aac09
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-05-09 20:27:14 -0600

    Update headers.py
    Added help_menu and pretty_banner

---
+++ headers.py
@@ -0,0 +1,50 @@
+help_menu = '''
+
+	-sf, --sniff :: Actively monitor traffic using PyShark
+	
+	-sp, --snoop :: Historically monitor traffic using net_connections and /dev/net/proc
+	 	      
+	
+	-o, --output :: Use if desiring output saved to a file, please pass your desired filename
+			
+			If no filename is passed, default will be standard date
+			
+	-h, --hidden :: Hide terminal output, only save to file
+	
+	-to, --timeout :: Set a timeout delay (Range 1-1000)
+	
+	-q, --quick :: Skip all aesthetic effects
+	
+	-h, --help :: Show this menu
+	
+	--credits :: roll credits
+	
+	--debug :: Shows various developing cues used in production
+	
+
+
+'''
+
+pretty_banner = '''                  
+  L.                                                       
+  EW:        ,ft                                           
+  E##;       t#E                 ..       :       GEEEEEEEL
+  E###t      t#E                ,W,     .Et       ,;;L#K;;.
+  E#fE#f     t#E               t##,    ,W#t          t#E   
+  E#t D#G    t#E              L###,   j###t          t#E   
+  E#t  f#E.  t#E            .E#j##,  G#fE#t          t#E   
+  E#t   t#K: t#E           ;WW; ##,:K#i E#t          t#E   
+  E#t    ;#W,t#E          j#E.  ##f#W,  E#t          t#E   
+  E#t     :K#D#E        .D#L    ###K:   E#t          t#E   
+  E#t      .E##E       :K#t     ##D.    E#t          t#E   
+  ..         G#E       ...      #G      ..            fE   
+              fE                j                      :   
+               ,                                                         
+	  _                               _____ , __    _           
+	 | |               o        ()        //|/  \ |(_\  /       
+	 | |           __           /\       /  |___/ |   \/        
+	 |/ \_|   |   /    |       /  \/    /   | \   |   /\   /\/  
+	  \_/  \_/|/  \___/|/      \__/\   /    |  \_/o _/  \_/ /\_/
+	         /|       /|                                        
+	         \|       \|_____                                                     
+'''
