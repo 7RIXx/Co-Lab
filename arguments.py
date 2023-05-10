3a79522afbf6214dbbedbafa3261509ebe032efd
7R!XxSec <62630479+7RIXx@users.noreply.github.com>
2023-05-09 20:22:35 -0600

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -2,26 +2,7 @@
 
 import argparse, psutil, os
 
-help_menu = '''
 
-	PLACEHOLDER FOR ASCII ART
-
-	
-	-to, --timeout :: Set a timeout delay (Range 1-1000)
-
-	-i, --interface :: Set a network interface card
-	
-	-h, --help :: Show this menu
-	
-	--debug :: Shows various developing cues used in production
-	
-	
-
-
-
-
-
-'''
 
 
 
@@ -32,14 +13,41 @@ help_menu = '''
 # build argument parser
 parser = argparse.ArgumentParser(add_help=False)
 
-parser.add_argument('-to', '--timeout', type=int, default=5)
-parser.add_argument('-i', '--interface', type=str, default='wlan0')
+parser.add_argument('-sf', '--sniff', action='store_true')
+parser.add_argument('-sp', '--snoop', action='store_true')
+parser.add_argument('-o', '--output', type=str)
+parser.add_argument('-H', '--hidden', action='store_true')
+parser.add_argument('-to', '--timeout', type=int)
+parser.add_argument('-q', '--quick', action='store_true')
 parser.add_argument('-h', '--help', action='store_true')
+parser.add_argument('--credits', action='store_true')
 parser.add_argument('--debug', action='store_true')
 
 # parse passed argumentation
 args = parser.parse_args()
 
+if args.credits:
+	helpers.roll_credits()
+	exit()
+
+if args.sniff and args.snoop is not None:
+	print(f' Please choose either Sniff or Snoop ')
+	exit()
+
+if args.output is not None:
+	try:
+		args.output = args.output.split('.')[0]
+	except:
+		pass
+
 if args.timeout is not None and args.timeout not in range(1,1000):
 	print(f'\n Timeout must be between 1 and 1000 \n')
 	exit()
+elif args.timeout is not None:
+	args.timeout = int(args.timeout)
+else:
+	args.timeout = 512
+
+
+
+
